fa36c37a568102b81615aa8323ddcf045d1af5f1
Thu Nguyen <ngthu1995@gmail.com>
2019-04-03 18:00:06 -0400

    complete wiring up the views with back-end code

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,29 @@
 console.log("Client side js loaded");
 
-fetch("http://localhost:3000/weather?address=rockville").then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      return console.log(data.error);
-    }
+const weatherForm = document.querySelector("form");
 
-    console.log(data.location);
-    console.log(data.forecast);
+const search = document.querySelector("input");
+
+const messageOne = document.querySelector("#message-1");
+
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const location = search.value;
+  console.log(location);
+
+  messageOne.textContent = "Loading ...";
+  messageTwo.textContent = "";
+
+  fetch("http://localhost:3000/weather?address=" + location).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        return (messageOne.textContent = data.error);
+      }
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecast;
+    });
   });
 });
